c59e1b4b88b6f500db0ea106135b4c13b97814c7
Philipp Holz <phil.epex@googlemail.com>
2018-02-26 15:23:37 +0100

    add aliases for zsh and tweak zshrc config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
-alias reload!='. ~/.zshrc'
+alias reloadzsh!=". ~/.zshrc && echo 'Your zshrc file has been reloaded'"
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias zshconfig="vim ~/.zshrc"
+alias ohmyzsh="vim ~/.oh-my-zsh"
