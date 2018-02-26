1ec2ced649cfafaf5b4cc02d5b1e737f0574139d
Philipp Holz <phil.epex@googlemail.com>
2018-02-26 15:32:21 +0100

    add java

---
+++ java/aliases.zsh
@@ -0,0 +1,3 @@
+alias java6='export JAVA_HOME="/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home"'
+alias java7='export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home"'
+alias java8='export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home"'
