59285414ec66212295ab09e1f1afd0d8cc22dc24
Philipp Holz <phil.epex@googlemail.com>
2018-02-26 15:16:29 +0100

    add my own git aliases and modfy gitconfig to my personal needs

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -22,3 +22,26 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+
+alias g="git"
+alias gs="git s"
+alias ga="git add"
+alias gaa="git add --all :/"
+alias gd="git difftool"
+alias gc="git commit -m"
+alias gca="git commit -am"
+alias gcaa="git add --all :/ && git commit -m"
+alias gp="git push"
+alias gpu="git pull"
+alias gpo="git push origin"
+alias gch="git checkout"
+alias grh="git reset --hard"
+alias grhod="git reset --hard origin/develop"
+alias gmerge="git merge"
+alias gmod="git merge origin/develop"
+alias gfo="git fetch origin"
+
+alias ggmail='git config user.name "Philipp Holz";git config user.email phil.epex@googlemail.com'
+alias gwork='git config user.name "Philipp Holz";git config user.email philipp.holz@unic.com'
+
+alias git_del_merged='git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d'
