1da02a3de9fcd0ac3c1ccef41a87f416738d9b54
Philipp Holz <phil.epex@googlemail.com>
2019-11-21 17:57:57 +0100

    iterm: do not open solarized theme on install

--- iterm2/install.zsh
+++ iterm2/install.zsh
@@ -3,7 +3,7 @@
 ###############################################################################
 
 # Install the Solarized Dark theme for iTerm
-open "~/.dotfiles/iterm2/themes/Solarized Dark - Patched.itermcolors"
+#open "~/.dotfiles/iterm2/themes/Solarized Dark - Patched.itermcolors"
 
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
