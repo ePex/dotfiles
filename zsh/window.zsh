60837a65339e08c69bb1653022dba5a7448d85fb
Philipp Holz <phil.epex@googlemail.com>
2019-11-21 18:34:11 +0100

    zsh: simplify prompt

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -1,19 +1,9 @@
-# From http://dotfiles.org/~_why/.zshrc
-# Sets the window title nicely no matter where you are
-function title() {
-  # escape '%' chars in $1, make nonprintables visible
-  a=${(V)1//\%/\%\%}
-
-  # Truncate command, and join lines.
-  a=$(print -Pn "%40>...>$a" | tr -d "\n")
-
-  case $TERM in
-  screen)
-    print -Pn "\ek$a:$3\e\\" # screen title (in ^A")
-    ;;
-  xterm*|rxvt)
-    print -Pn "\e]2;$2\a" # plain xterm title ($3 for pwd)
-    ;;
-  esac
+function git_branch {
+  BRANCH_REFS=$(git symbolic-ref HEAD 2>/dev/null) || return
+  GIT_BRANCH="${BRANCH_REFS#refs/heads/}"
+  [ -n "$GIT_BRANCH" ] && echo "($GIT_BRANCH)"
 }
 
+function precmd {
+  echo -ne "\e]1;${PWD//*\//} $(git_branch)\a"
+}
