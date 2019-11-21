7e0b76a3ed6faa59f7db89f31a3783ea9822128f
Philipp Holz <phil.epex@googlemail.com>
2019-11-21 17:49:23 +0100

    adjusts ant opts and adds hybris environment variables

--- ant/path.zsh
+++ ant/path.zsh
@@ -1,3 +1,13 @@
-export ANT_OPTS="-Xmx2048m -XX:MaxPermSize=1024m"
+export PLATFORM_HOME="$HOME/Projects/hybris/bin/platform"
+export ANT_OPTS="-Xmx512m -Dfile.encoding=UTF-8"
+export ANT_HOME="$PLATFORM_HOME/apache-ant-1.9.1"
+chmod +x "$ANT_HOME/bin/ant"
+chmod +x "$PLATFORM_HOME/license.sh"
+export PATH="$ANT_HOME/bin:$PATH"
+
+#export ANT_OPTS="-Xmx2048m -XX:MaxPermSize=1024m"
+#export ANT_HOME="$HOME/Projects/hybris/bin/platform/apache-ant-1.9.1"
+#export PATH="$PATH:$ANT_HOME/bin"
+
 #export ANT_HOME=/opt/ant/ant
 #export PATH="$ANT_HOME/bin:$PATH"
