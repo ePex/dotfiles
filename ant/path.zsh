354d40512bac0280e14b2415b04f0eab6cb4f917
Philipp Holz <phil.epex@googlemail.com>
2020-04-02 09:32:01 +0200

    set ant opts only if hybris project exists

--- ant/path.zsh
+++ ant/path.zsh
@@ -1,9 +1,12 @@
-export PLATFORM_HOME="$HOME/Projects/hybris/bin/platform"
-export ANT_OPTS="-Xmx512m -Dfile.encoding=UTF-8"
-export ANT_HOME="$PLATFORM_HOME/apache-ant-1.9.1"
-chmod +x "$ANT_HOME/bin/ant"
-chmod +x "$PLATFORM_HOME/license.sh"
-export PATH="$ANT_HOME/bin:$PATH"
+if test -d "$HOME/Projects/hybris"
+then
+    export PLATFORM_HOME="$HOME/Projects/hybris/bin/platform"
+    export ANT_OPTS="-Xmx512m -Dfile.encoding=UTF-8"
+    export ANT_HOME="$PLATFORM_HOME/apache-ant-1.9.1"
+    chmod +x "$ANT_HOME/bin/ant"
+    chmod +x "$PLATFORM_HOME/license.sh"
+    export PATH="$ANT_HOME/bin:$PATH"
+fi
 
 #export ANT_OPTS="-Xmx2048m -XX:MaxPermSize=1024m"
 #export ANT_HOME="$HOME/Projects/hybris/bin/platform/apache-ant-1.9.1"
