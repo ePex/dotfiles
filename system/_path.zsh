07013cb7e6e82b2bc20e06e0d2bf5e31242dbbfa
Philipp Holz <phil.epex@googlemail.com>
2020-05-29 09:49:03 +0200

    removes duplicate exports and set default editor to vim

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,15 +1,2 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-# Preferred editor for local and remote sessions
-if [[ -n $SSH_CONNECTION ]]; then
-    export EDITOR='vim'
-else
-    export EDITOR='atom'
-fi
-
-# You may need to manually set your language environment
-export LANG=en_US.UTF-8
-
-# ssh
-export SSH_KEY_PATH="$HOME/.ssh/rsa_id"
--- system/env.zsh
+++ system/env.zsh
@@ -1,9 +1,4 @@
-# Preferred editor for local and remote sessions
-if [[ -n $SSH_CONNECTION ]]; then
-    export EDITOR='vim'
-else
-    export EDITOR='atom'
-fi
+export EDITOR='vim'
 
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
