881909853adb734388a1c7ea4d54d152bc88f0c7
Philipp Holz <phil.epex@googlemail.com>
2021-03-23 09:54:43 +0100

    adds some git aliases to clean (stale) branches

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -45,3 +45,7 @@ alias ggmail='git config user.name "Philipp Holz";git config user.email phil.epe
 alias gwork='git config user.name "Philipp Holz";git config user.email philipp.holz@codesupply.de'
 
 alias git_del_merged='git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d'
+alias gitcleanlocal_alreadymerged='git checkout master && git fetch && git remote prune origin && git branch --merged master | egrep -v '\''[^-]master$'\'' | xargs git branch -d'
+alias gitcleanlocal_alreadymerged_develop='git checkout develop && git fetch && git remote prune origin && git branch --merged develop | egrep -v '\''[^-]develop$'\'' | xargs git branch -d'
+alias gitcleanlocal_gone='git branch -vv | grep "gone" | cut -f3 -d\  | xargs git branch -D'
+alias bclean = "!f() { branches=$(git branch --merged ${1-master} | grep -v " ${1-master}$"); [ -z \"$branches\" ] || git branch -d $branches; }; f"
